e8f69a7671c37ed76c0665e5e596b03ea458a96b
luis castillo <lc77278@gmail.com>
2021-07-01 03:17:07 -0500

    modify app.js

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT ||  3000, () => {
     console.log('servidor 3000 activo');
 });
